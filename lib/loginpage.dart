e1b18e424b56b4679c2a1a9e9b2ba2874b61f573
Giulia <giuliadanso@gmail.com>
2023-11-09 16:44:02 +0100

    initial commit

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -35,34 +35,42 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //login UI here
-    return Container(
-      // login screen content
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const TextField(
-            decoration: InputDecoration(
-              labelText: 'Username',
-              border: OutlineInputBorder(),
-            ),
+    return MaterialApp(
+      home: Scaffold(
+        // login screen content
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 24.0),
+              ElevatedButton(
+                onPressed: () {
+                  handleLogin(context);
+                  print('Login button pressed');
+                },
+                child: Text('Login'),
+              ),
+            ],
           ),
-          const SizedBox(height: 16.0),
-          const TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          const SizedBox(height: 24.0),
-          ElevatedButton(
-            onPressed: () {
-              handleLogin(context);
-            },
-            child: const Text('Login'),
-          )
-        ],
+        ),
       ),
     );
   }
